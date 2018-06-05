3973dc1562b0e5093c7e0d2ee3cf09e39d6f5ca9
Monica <mtucker13@student.gsu.edu>
2018-06-04 23:51:15 -0400

    Initial commit

---
+++ app/src/main/java/edu/edu/httpsgsu/a2018summerex/MainActivity.java
@@ -0,0 +1,19 @@
+package edu.edu.httpsgsu.a2018summerex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(context: this, text:"Hello Android!", Toast.LENGTH_LONG).show();
+
+    }
+
+}
